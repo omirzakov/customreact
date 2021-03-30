79f65499c26e146aba7f785b1f719843c224838e
omirzakov <huistowns@gmail.com>
2021-03-30 19:11:13 +0600

    first commit

---
+++ src/components/app.component.js
@@ -0,0 +1,16 @@
+import React from "react";
+import s from "./app.component.css";
+
+
+const App = () => {
+
+    return (
+        <div className={s.intro}>
+            <div>
+
+                Home my home
+            </div>
+        </div>
+    )
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    mode: "development",
+    output: {
+        filename: "./main.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000,
+        watchContentBase: true,
+        progress: true
+    },
+}
